c24db27da581a641ed05abca429c2c7ac8cc3182
Guohao Jiao <97696069+imgavin777@users.noreply.github.com>
2022-04-08 08:26:58 -0400

    Update func_test.py

--- func_test.py
+++ func_test.py
@@ -1,10 +1,15 @@
 import unittest
+from unittest.mock import MagicMock, patch
+from app import login
 
 class Tests (unittest.Testcase):
-    def test(self):
-        pass    
+    def test_login(self):
+        mock_response = MagicMock()
+        mock_response.json.return_value = {}
         
-
-
+        with patch ("app.request.get") as mock_requests_get:
+            mock_requests_get.return_value = mock_response
+            result = login()
+            self.assertEqual(result,"google.com")
 if __name__ == "__main__":
     unittest.main()
